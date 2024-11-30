309913c32106eadb05dc771b5631e103ecc92872
Theo Billardey <toto.billardey@gmail.com>
2024-11-30 16:08:24 +0100

    feat(cars): create page

---
+++ src/app/cars/constants.ts
@@ -0,0 +1,10 @@
+import type { SelectOption } from "@/components/Select";
+
+export const carSortOptionList: SelectOption[] = [
+  { label: "Price - low to high", value: "price-asc" },
+  { label: "Price - high to low", value: "price-desc" },
+  { label: "Mileage - low to high", value: "miles-asc" },
+  { label: "Mileage - high to low", value: "miles-desc" },
+  { label: "Year - Oldest first", value: "year-asc" },
+  { label: "Year - Most recent first", value: "year-desc" },
+];

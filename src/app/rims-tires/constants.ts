2519c80b515749649656df8837f9553af0422b09
Theo Billardey <toto.billardey@gmail.com>
2025-01-10 16:43:49 +0100

    feat(rims-tires): create constants

---
+++ src/app/rims-tires/constants.ts
@@ -0,0 +1,6 @@
+import type { SelectOption } from "@/components/Select";
+
+export const wheelSortOptionList: SelectOption[] = [
+  { label: "Price - low to high", value: "price-asc" },
+  { label: "Price - high to low", value: "price-desc" },
+];

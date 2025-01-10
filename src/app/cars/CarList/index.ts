c3f3ff0123ec5973aaf33cbacf4179df64069aa6
Theo Billardey <toto.billardey@gmail.com>
2025-01-10 16:42:18 +0100

    feat(components): create separate lists
    Delete generic List component and instead create 2 lib related lists

---
+++ src/app/cars/CarList/index.ts
@@ -0,0 +1,5 @@
+import CarList from "./CarList";
+import { CarListProps } from "./carlist";
+
+export type { CarListProps };
+export default CarList;
---
+++ src/app/rims-tires/WheelList/index.ts
@@ -0,0 +1,5 @@
+import WheelList from "./WheelList";
+import { WheelListProps } from "./wheellist";
+
+export type { WheelListProps };
+export default WheelList;

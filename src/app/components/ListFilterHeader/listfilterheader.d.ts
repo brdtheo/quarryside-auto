5815ff7faabb6eb65806b43f2c5e6884ab8522d8
Theo Billardey <toto.billardey@gmail.com>
2025-01-10 17:53:06 +0100

    refactor(components): generic list filter header
    Remove hardcoded data and apply proper props

--- src/app/components/ListFilterHeader/listfilterheader.d.ts
+++ src/app/components/ListFilterHeader/listfilterheader.d.ts
@@ -1,5 +1,12 @@
 import type { SelectOption } from "@/components/Select";
 
+type AppliedFilter = {
+  name: string;
+  value: string;
+};
+
 export type ListFilterHeaderProps = {
+  textSearch: string;
   sortOptionList: SelectOption[];
+  appliedFilterList: AppliedFilter[];
 };

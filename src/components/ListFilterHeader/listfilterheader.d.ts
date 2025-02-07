8eb1399d61ad7d7d95700f5f2efd67f44cec2da9
Theo Billardey <toto.billardey@gmail.com>
2025-02-07 16:34:51 +0100

    feat(components): exclude applied filter list from header
    Remove applied filters sections from list filter header

--- src/components/ListFilterHeader/listfilterheader.d.ts
+++ src/components/ListFilterHeader/listfilterheader.d.ts
@@ -1,12 +1,6 @@
 import type { SelectOption } from "@/components/Select";
 
-type AppliedFilter = {
-  name: string;
-  value: string;
-};
-
 export type ListFilterHeaderProps = {
   textSearch: string;
   sortOptionList: SelectOption[];
-  appliedFilterList: AppliedFilter[];
 };

a561502fc4d0c26f00dc7d0d6983090bb949f888
Theo Billardey <toto.billardey@gmail.com>
2025-02-07 12:41:48 +0100

    refactor(components): update and handle Chip props

--- src/components/Chip/chip.d.ts
+++ src/components/Chip/chip.d.ts
@@ -1,4 +1,17 @@
+import { ReactNode } from "react";
+
+import type { Url } from "@/types";
+
 export type ChipProps = {
   children: string;
-  onClick: () => void;
+  leftIcon?: ReactNode;
+  rightIcon?: ReactNode;
+  /** If passed, the path is set to a Link element */
+  iconHref?: Url;
+};
+
+export type ChipIconProps = {
+  className?: string;
+  children: ReactNode;
+  href?: Url;
 };

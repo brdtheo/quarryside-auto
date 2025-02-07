f61662ca8a564960b8bc1674c676b5ba340d8ba9
Theo Billardey <toto.billardey@gmail.com>
2025-02-07 17:39:11 +0100

    feat(wheel): create pagination page size constant

--- src/lib/wheel/constants.ts
+++ src/lib/wheel/constants.ts
@@ -4,3 +4,5 @@ export const wheelSortOptionList: SelectOption[] = [
   { label: "Price - low to high", value: "price-asc" },
   { label: "Price - high to low", value: "price-desc" },
 ];
+
+export const WHEEL_LIST_PAGE_SIZE = 20;

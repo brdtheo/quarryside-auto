0fb3fa1db8d238be954cc30bc42887c4a923e255
Theo Billardey <toto.billardey@gmail.com>
2025-02-07 12:40:23 +0100

    refactor(components): set checkbox component as Link

--- src/components/Checkbox/Checkbox.tsx
+++ src/components/Checkbox/Checkbox.tsx
@@ -1,18 +1,16 @@
 "use client";
 
+import Link from "next/link";
+
 import { IconCheck } from "@tabler/icons-react";
 
 import clsx from "clsx";
 
 import type { CheckboxProps } from ".";
 
-export default function Checkbox({
-  id,
-  checked,
-  label,
-}: CheckboxProps) {
+export default function Checkbox({ id, checked, label, href }: CheckboxProps) {
   return (
-    <div className="flex gap-3 items-center select-none cursor-pointer w-fit">
+    <Link className="flex gap-3 items-center select-none w-fit" href={href}>
       <div className="relative">
         <input
           id={id}
@@ -20,7 +18,7 @@ export default function Checkbox({
           type="checkbox"
           name={id}
           checked={checked}
-          onChange={() => {}}
+          readOnly
         />
         <div
           className={clsx("w-4 h-4 rounded border flex items-center", {
@@ -40,6 +38,6 @@ export default function Checkbox({
       <label className="text-sm cursor-pointer" htmlFor={id}>
         {label}
       </label>
-    </div>
+    </Link>
   );
 }
--- src/components/Checkbox/checkbox.d.ts
+++ src/components/Checkbox/checkbox.d.ts
@@ -1,8 +1,6 @@
-import { ChangeEventHandler } from "react";
-
 export type CheckboxProps = {
   id: string;
   label: string;
   checked: boolean;
-  onChange?: ChangeEventHandler;
+  href: string;
 };

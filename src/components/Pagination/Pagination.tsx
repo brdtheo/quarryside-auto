a9dd7379c7bc577bc528e8a52acb7e8022496b46
Theo Billardey <toto.billardey@gmail.com>
2025-02-07 14:31:51 +0100

    feat(components): improve pagination logic and display

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,28 +1,71 @@
+"use client";
+
+import Link from "next/link";
+
+import { IconChevronsLeft, IconChevronsRight } from "@tabler/icons-react";
+
 import clsx from "clsx";
+
+import useURLSearchParams from "@/hooks/useURLSearchParams";
 
 import type { PaginationProps } from ".";
 
 export default function Pagination({
-  page,
+  page = 1,
   pageCount,
-  disabled,
+  searchParams,
 }: PaginationProps) {
+  const pageIndexThreshold = 2;
   const pages = [...Array(pageCount).keys()].map((page) => page + 1);
+
+  const lastPage = pages.length;
+
+  const { getUpdatedURLFromSearchParam } = useURLSearchParams(searchParams);
+
   return (
-    <div className="flex flex-1 justify-center">
-      {(pages ?? []).map((currentPage, index) => (
-        <button
-          disabled={disabled}
-          className={clsx("w-7 h-7 rounded", {
-            "hover:bg-grey transition-colors duration-100":
-              currentPage !== page,
-            "bg-brown text-white": currentPage === page,
-          })}
-          key={index}
+    <div className="flex flex-1 justify-center gap-1">
+      {page !== 1 && (
+        <Link
+          href={getUpdatedURLFromSearchParam("page", "1")}
+          className="w-7 h-7 rounded inline-flex items-center justify-center hover:bg-grey transition-colors duration-100"
         >
-          {currentPage}
-        </button>
-      ))}
+          <IconChevronsLeft strokeWidth={1.5} size={20} />
+        </Link>
+      )}
+
+      {(pages ?? []).map((currentPage, index) => {
+        if (
+          currentPage < page - pageIndexThreshold ||
+          currentPage > page + pageIndexThreshold
+        ) {
+          return null;
+        }
+        return (
+          <Link
+            href={getUpdatedURLFromSearchParam("page", `${currentPage}`)}
+            className={clsx(
+              "w-7 h-7 rounded inline-flex items-center justify-center",
+              {
+                "hover:bg-grey transition-colors duration-100":
+                  currentPage !== page,
+                "bg-brown text-white": currentPage === page,
+              },
+            )}
+            key={index}
+          >
+            {currentPage}
+          </Link>
+        );
+      })}
+
+      {page !== lastPage && (
+        <Link
+          href={getUpdatedURLFromSearchParam("page", `${lastPage}`)}
+          className="w-7 h-7 rounded inline-flex items-center justify-center hover:bg-grey transition-colors duration-100"
+        >
+          <IconChevronsRight strokeWidth={1.5} size={20} />
+        </Link>
+      )}
     </div>
   );
 }

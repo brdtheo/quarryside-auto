8ff11dbdb389ce4359affd010545ea68ab8c6905
Theo Billardey <toto.billardey@gmail.com>
2025-02-07 16:53:09 +0100

    feat(vehicle): handle prisma data

--- src/app/vehicles/page.tsx
+++ src/app/vehicles/page.tsx
@@ -1,34 +1,50 @@
-import { Vehicle } from "@prisma/client";
+import { Prisma, Vehicle } from "@prisma/client";
 
 import Container from "@/components/Container";
-import ListFilterAside from "@/components/ListFilterAside";
 import ListFilterHeader from "@/components/ListFilterHeader";
 import Pagination from "@/components/Pagination";
 
 import { prisma } from "@/lib/prisma";
 import VehicleCard from "@/lib/vehicle/VehicleCard";
 import VehicleList from "@/lib/vehicle/VehicleList";
-import { vehicleSortOptionList } from "@/lib/vehicle/constants";
+import VehicleListFilterAside from "@/lib/vehicle/VehicleListFilterAside";
+import {
+  VEHICLE_LIST_PAGE_SIZE,
+  vehicleSortOptionList,
+} from "@/lib/vehicle/constants";
+import { getVehicleFindManyArgs } from "@/lib/vehicle/utils";
 
-export default async function Page() {
-  const vehicles = await prisma.vehicle.findMany({ take: 15 });
+import type { PageProps } from "@/types";
+
+export default async function Page({ searchParams }: PageProps) {
+  const params = await searchParams;
+  const page = params?.page ? parseInt(params?.page as string) : 1;
+
+  const prismaFindManyArgs = getVehicleFindManyArgs(params);
+  const prismaFindManyCountArgs = getVehicleFindManyArgs(params, true);
+
+  const vehicles = await prisma.vehicle.findMany(
+    prismaFindManyArgs as Prisma.VehicleFindManyArgs,
+  );
+  const vehiclesCount =
+    (await prisma.vehicle.count(
+      prismaFindManyCountArgs as Prisma.VehicleCountArgs,
+    )) ?? 0;
 
   return (
     <Container className="m-auto gap-8 flex flex-col py-8">
       <h1 className="font-bold text-2xl">Used vehicles</h1>
 
       <div className="flex gap-8">
-        <ListFilterAside />
+        <VehicleListFilterAside searchParams={params} />
 
         <div className="flex flex-1 flex-col">
           <ListFilterHeader
-            textSearch="etk 800"
-            appliedFilterList={[
-              { name: "q", value: "etk 800" },
-              { name: "condition", value: "Used" },
-            ]}
+            textSearch=""
             sortOptionList={vehicleSortOptionList}
+            resultCount={vehiclesCount}
           />
+
           <VehicleList
             data={vehicles}
             itemRender={(vehicle: Vehicle) => (
@@ -49,7 +65,11 @@ export default async function Page() {
         </div>
       </div>
 
-      <Pagination page={1} pageCount={5} onPageChange={() => {}} />
+      <Pagination
+        searchParams={params}
+        page={page}
+        pageCount={Math.ceil(vehiclesCount / VEHICLE_LIST_PAGE_SIZE) || 1}
+      />
     </Container>
   );
 }

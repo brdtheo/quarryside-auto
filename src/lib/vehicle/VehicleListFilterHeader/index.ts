f32dc3e340b5effe4f58bcf09d0af7059c83c2c6
Theo Billardey <toto.billardey@gmail.com>
2025-02-10 17:55:59 +0100

    feat(vehicle): create list filter header component

---
+++ src/lib/vehicle/VehicleListFilterHeader/index.ts
@@ -0,0 +1,5 @@
+import VehicleListFilterHeader from "./VehicleListFilterHeader";
+import type { VehicleListFilterHeaderProps } from "./vehiclelistfilterheader";
+
+export type { VehicleListFilterHeaderProps };
+export default VehicleListFilterHeader;

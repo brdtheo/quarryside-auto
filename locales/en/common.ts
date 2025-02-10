39da1d8114e4491ef9589ab953b62f4a8306424a
Theo Billardey <toto.billardey@gmail.com>
2025-02-10 16:44:49 +0100

    config(i18n): create translation files

---
+++ locales/en/common.ts
@@ -0,0 +1,64 @@
+export default {
+  reviews: "Reviews",
+  search: "Search",
+  resultCount: "{{ count }} result(s)",
+  sortResults: {
+    title: "Sort results",
+    option: {
+      priceAsc: "Price - low to high",
+      priceDesc: "Price - high to low",
+      mileageAsc: "Mileage - low to high",
+      mileageDesc: "Mileage - high to low",
+      yearAsc: "Year - oldest first",
+      yearDesc: "Year - most recent first",
+    },
+  },
+  header: {
+    myCart: "My Cart",
+    account: "Account",
+  },
+  subHeader: {
+    usedVehicles: "Used vehicles",
+    rimsTires: "Rims & tires",
+    careers: "Careers",
+    helpCenter: "Help center",
+    about: "About",
+  },
+  footer: {
+    section: {
+      usedVehicles: {
+        title: "Used vehicles",
+        usedCivettaCars: "Used Civetta cars",
+        usedIbishuCars: "Used Ibishu cars",
+        usedGavrilCars: "Used Gavril cars",
+        usedEtkCars: "Used ETK cars",
+        usedHiroshiCars: "Used Hiroshi cars",
+      },
+      rimsTires: {
+        title: "Rims & tires",
+        autobelloWheels: "Autobello wheels",
+        etkWheels: "ETK wheels",
+        folkWheels: "Folk wheels",
+        gavrilWheels: "Gavril wheels",
+        ibishuWheels: "Ibishu wheels",
+      },
+      careers: {
+        title: "Careers",
+        openPositions: "Open positions",
+        internshipProgram: "Internship program",
+        carJockeyRequirements: "Car jockey requirements",
+      },
+      helpCenter: {
+        title: "Help Center",
+        questionAnswers: "Q&As",
+        delivery: "Delivery",
+        contact: "Contact",
+      },
+      about: {
+        title: "About",
+        ourCompany: "Our company",
+        whereToFindUs: "Where to find us",
+      },
+    },
+  },
+};
---
+++ locales/ru/common.ts
@@ -0,0 +1,64 @@
+export default {
+  reviews: "отзывы покупателей",
+  search: "обыскивать",
+  resultCount: "{{ count }} результат(ы)",
+  sortResults: {
+    title: "Сортировка результатов",
+    option: {
+      priceAsc: "Цена - от низкой до высокой",
+      priceDesc: "Цена - от высокой до низкой",
+      mileageAsc: "Пробег - от низкого до высокого",
+      mileageDesc: "Пробег - от большого до малого",
+      yearAsc: "Год - самый старый первый",
+      yearDesc: "Год - последний первый",
+    },
+  },
+  header: {
+    myCart: "моя тележка",
+    account: "аккаунт",
+  },
+  subHeader: {
+    usedVehicles: "Подержанные автомобили",
+    rimsTires: "Обода и шины",
+    careers: "Карьера",
+    helpCenter: "Центр помощи",
+    about: "предложение",
+  },
+  footer: {
+    section: {
+      usedVehicles: {
+        title: "Подержанные автомобили",
+        usedCivettaCars: "Подержанные автомобили Civetta",
+        usedIbishuCars: "Подержанные автомобили Ibishu",
+        usedGavrilCars: "Подержанные автомобили Gavril",
+        usedEtkCars: "Подержанные автомобили ETK",
+        usedHiroshiCars: "Подержанные автомобили Hiroshi",
+      },
+      rimsTires: {
+        title: "Обода и шины",
+        autobelloWheels: "Колеса Autobello",
+        etkWheels: "Колеса ETK",
+        folkWheels: "Колеса Folk",
+        gavrilWheels: "Колеса Gavril",
+        ibishuWheels: "Колеса Ibishu",
+      },
+      careers: {
+        title: "Карьера",
+        openPositions: "Открытые позиции",
+        internshipProgram: "Программа стажировки",
+        carJockeyRequirements: "Требования к жокею автомобиля",
+      },
+      helpCenter: {
+        title: "Центр помощи",
+        questionAnswers: "вопросы и ответы",
+        delivery: "Доставка",
+        contact: "Связаться с",
+      },
+      about: {
+        title: "о нашей компании",
+        ourCompany: "Наша компания",
+        whereToFindUs: "Где нас найти",
+      },
+    },
+  },
+};

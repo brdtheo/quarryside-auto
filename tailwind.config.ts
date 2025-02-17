db747aae249a7ea35d9bf0cc865edbe8c07b75d0
Theo Billardey <toto.billardey@gmail.com>
2025-02-17 15:04:12 +0100

    config(tailwind): revamp color palette

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,13 +10,16 @@ export default {
   theme: {
     extend: {
       colors: {
-        black: "#050505",
-        grey: "#EBEBEB",
-        "grey-secondary": "#9B9B9B",
-        brown: "#2F2012",
-        "brown-secondary": "#584c41",
+        primary: "#312011",
+        primarydark: "#eeddce",
+        secondary: "#b7a995",
+        secondarydark: "#6a5c48",
         background: "#F8F9FD",
+        backgrounddark: "#020308",
+        black: "#020308",
+        blacksecondary: "hsla(0,0%,100%,0.14)",
         divider: "#E1E1E1",
+        dividerdark: "#1e1e1e",
         yellow: "#FFE714",
         red: "#CF061F",
         "ad-background": "#CDCCCC",
@@ -25,5 +28,6 @@ export default {
       borderRadius: { DEFAULT: "4px" },
     },
   },
+  darkMode: "class",
   plugins: [],
 } satisfies Config;

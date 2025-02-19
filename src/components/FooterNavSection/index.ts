52ec1fab9c5bc52ce610ee6805a0dede40c07db5
Theo Billardey <toto.billardey@gmail.com>
2025-02-19 15:47:41 +0100

    refactor(components): footer sections naming

--- src/components/FooterNavSection/index.ts
+++
@@ -1,5 +0,0 @@
-import FooterNavSection from "./FooterNavSection";
-import { FooterNavSection as FooterNavSectionProps } from "./footernavsection";
-
-export type { FooterNavSectionProps };
-export default FooterNavSection;

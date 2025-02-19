9d69af996e6645f4badad651a386e9f9e146e326
Theo Billardey <toto.billardey@gmail.com>
2025-02-19 15:48:27 +0100

    feat(components): create side drawer

---
+++ src/components/SideDrawer/SideDrawer.tsx
@@ -0,0 +1,26 @@
+import { IconX } from "@tabler/icons-react";
+
+import IconButton from "@/components/IconButton";
+
+import type { SideDrawerProps } from ".";
+
+export default function SideDrawer({
+  children,
+  isOpen,
+  onClose,
+}: SideDrawerProps) {
+  return (
+    isOpen && (
+      <div className="w-dvw h-dvh fixed top-0 left-0 z-10">
+        <button className="bg-black opacity-80 w-dvw h-dvh" onClick={onClose} />
+        <div className="w-60 bg-background border-l border-l-divider fixed top-0 right-0 h-dvh px-4 py-6 gap-6 flex flex-col overflow-y-scroll">
+          <IconButton className="border border-divider" onClick={onClose}>
+            <IconX size={20} stroke={2} />
+          </IconButton>
+
+          {children}
+        </div>
+      </div>
+    )
+  );
+}

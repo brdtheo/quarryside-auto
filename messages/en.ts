591c6c436bc77bcc60200ca74866a4a15f332ef5
Theo Billardey <toto.billardey@gmail.com>
2025-02-20 16:17:33 +0100

    config(i18n): create next-intl messages

---
+++ messages/en.ts
@@ -0,0 +1,5 @@
+import common from "../locales/en/common";
+import vehicles from "../locales/en/vehicles";
+import wheels from "../locales/en/wheels";
+
+export default { ...common, ...vehicles, ...wheels };

581e4c24d56debdb2fdf3e2c6167d17b00f30601
Theo Billardey <toto.billardey@gmail.com>
2025-02-20 19:09:25 +0100

    chore(i18n): add namespace key in JSON files

--- locales/en/wheels.ts
+++ locales/en/wheels.ts
@@ -1,82 +1,84 @@
 import { WheelBrand } from "@prisma/client";
 
 export default {
-  title: "Rims & tires",
-  availability: "Availability",
-  availableOn: "Available on",
-  addToCart: "Add to cart",
-  assemblyWithoutAppointment: "Assembly without appointment",
-  details: {
-    specifications: "Specifications",
-    availableSizes: {
-      title: "Available sizes",
-    },
-    availableTires: {
-      title: "Available tires",
-    },
-    consumption: {
-      title: "Consumption",
-    },
-  },
-  filter: {
-    brand: {
-      title: "Brand",
-      option: {
-        [WheelBrand.ALDER]: "Alder",
-        [WheelBrand.AUTOBELLO]: "Autobello",
-        [WheelBrand.BETA]: "Beta",
-        [WheelBrand.BRUCKELL]: "Bruckell",
-        [WheelBrand.CHERRIER]: "Cherrier",
-        [WheelBrand.CIVETTA]: "Civetta",
-        [WheelBrand.CLOCKWISE]: "Clockwise",
-        [WheelBrand.DREID]: "Dreid",
-        [WheelBrand.ETK]: "ETK",
-        [WheelBrand.FOLK]: "Folk",
-        [WheelBrand.GAVRIL]: "Gavril",
-        [WheelBrand.GP]: "GP",
-        [WheelBrand.GRIP_ALL]: "Grip-All",
-        [WheelBrand.HIROSHI]: "Hiroshi",
-        [WheelBrand.IBISHU]: "Ibishu",
-        [WheelBrand.LEGNO]: "Legno",
-        [WheelBrand.NO_BRAND]: "N/A",
-        [WheelBrand.OKUDAI]: "Okudai",
-        [WheelBrand.SOLIAD]: "Soliad",
-        [WheelBrand.SP]: "SP",
-        [WheelBrand.STX]: "STX",
-        [WheelBrand.TIMS]: "TIMS",
-        [WheelBrand.TRIAL]: "Trial",
-        [WheelBrand.UTR]: "UTR",
-        [WheelBrand.WANGAN]: "Wangan",
+  wheels: {
+    title: "Rims & tires",
+    availability: "Availability",
+    availableOn: "Available on",
+    addToCart: "Add to cart",
+    assemblyWithoutAppointment: "Assembly without appointment",
+    details: {
+      specifications: "Specifications",
+      availableSizes: {
+        title: "Available sizes",
+      },
+      availableTires: {
+        title: "Available tires",
+      },
+      consumption: {
+        title: "Consumption",
       },
     },
-    is_three_lug: {
-      option: { true: "3 lugs" },
-    },
-    is_four_lug: {
-      option: { true: "4 lugs" },
-    },
-    is_five_lug: {
-      option: { true: "5 lugs" },
-    },
-    is_six_lug: {
-      option: { true: "6 lugs" },
-    },
-    is_eight_lug: {
-      option: { true: "8 lugs" },
-    },
-    is_central_lug: {
-      option: { true: "Central lug" },
-    },
-    delivery_available: {
-      title: "Delivery",
-      option: {
-        true: "Available for delivery",
+    filter: {
+      brand: {
+        title: "Brand",
+        option: {
+          [WheelBrand.ALDER]: "Alder",
+          [WheelBrand.AUTOBELLO]: "Autobello",
+          [WheelBrand.BETA]: "Beta",
+          [WheelBrand.BRUCKELL]: "Bruckell",
+          [WheelBrand.CHERRIER]: "Cherrier",
+          [WheelBrand.CIVETTA]: "Civetta",
+          [WheelBrand.CLOCKWISE]: "Clockwise",
+          [WheelBrand.DREID]: "Dreid",
+          [WheelBrand.ETK]: "ETK",
+          [WheelBrand.FOLK]: "Folk",
+          [WheelBrand.GAVRIL]: "Gavril",
+          [WheelBrand.GP]: "GP",
+          [WheelBrand.GRIP_ALL]: "Grip-All",
+          [WheelBrand.HIROSHI]: "Hiroshi",
+          [WheelBrand.IBISHU]: "Ibishu",
+          [WheelBrand.LEGNO]: "Legno",
+          [WheelBrand.NO_BRAND]: "N/A",
+          [WheelBrand.OKUDAI]: "Okudai",
+          [WheelBrand.SOLIAD]: "Soliad",
+          [WheelBrand.SP]: "SP",
+          [WheelBrand.STX]: "STX",
+          [WheelBrand.TIMS]: "TIMS",
+          [WheelBrand.TRIAL]: "Trial",
+          [WheelBrand.UTR]: "UTR",
+          [WheelBrand.WANGAN]: "Wangan",
+        },
       },
-    },
-    free_on_site_pickup: {
-      title: "On site pickup",
-      option: {
-        true: "Free on site pickup",
+      is_three_lug: {
+        option: { true: "3 lugs" },
+      },
+      is_four_lug: {
+        option: { true: "4 lugs" },
+      },
+      is_five_lug: {
+        option: { true: "5 lugs" },
+      },
+      is_six_lug: {
+        option: { true: "6 lugs" },
+      },
+      is_eight_lug: {
+        option: { true: "8 lugs" },
+      },
+      is_central_lug: {
+        option: { true: "Central lug" },
+      },
+      delivery_available: {
+        title: "Delivery",
+        option: {
+          true: "Available for delivery",
+        },
+      },
+      free_on_site_pickup: {
+        title: "On site pickup",
+        option: {
+          true: "Free on site pickup",
+        },
       },
     },
   },
--- locales/ru/common.ts
+++ locales/ru/common.ts
@@ -1,66 +1,68 @@
 export default {
-  notFoundTitle: "ddd",
-  notFoundDescription:
-    "Похоже, что содержимое, которое вы ищете, не существует или было перемещено. Вместо этого наслаждайтесь этим прыгающим Covet:",
-  reviews: "отзывы покупателей",
-  search: "обыскивать",
-  resultCount: "{{ count }} результат(ы)",
-  sortResults: {
-    title: "Сортировка результатов",
-    option: {
-      priceAsc: "Цена - от низкой до высокой",
-      priceDesc: "Цена - от высокой до низкой",
-      mileageAsc: "Пробег - от низкого до высокого",
-      mileageDesc: "Пробег - от большого до малого",
-      yearAsc: "Год - самый старый первый",
-      yearDesc: "Год - последний первый",
+  common: {
+    notFoundTitle: "ddd",
+    notFoundDescription:
+      "Похоже, что содержимое, которое вы ищете, не существует или было перемещено. Вместо этого наслаждайтесь этим прыгающим Covet:",
+    reviews: "отзывы покупателей",
+    search: "обыскивать",
+    resultCount: "{ count } результат(ы)",
+    sortResults: {
+      title: "Сортировка результатов",
+      option: {
+        priceAsc: "Цена - от низкой до высокой",
+        priceDesc: "Цена - от высокой до низкой",
+        mileageAsc: "Пробег - от низкого до высокого",
+        mileageDesc: "Пробег - от большого до малого",
+        yearAsc: "Год - самый старый первый",
+        yearDesc: "Год - последний первый",
+      },
     },
-  },
-  header: {
-    myCart: "моя тележка",
-    account: "аккаунт",
-  },
-  subHeader: {
-    usedVehicles: "Подержанные автомобили",
-    rimsTires: "Обода и шины",
-    careers: "Карьера",
-    helpCenter: "Центр помощи",
-    about: "предложение",
-  },
-  navigation: {
-    home: "дом",
-    usedVehicles: {
-      title: "Подержанные автомобили",
-      usedCivettaCars: "Подержанные автомобили Civetta",
-      usedIbishuCars: "Подержанные автомобили Ibishu",
-      usedGavrilCars: "Подержанные автомобили Gavril",
-      usedEtkCars: "Подержанные автомобили ETK",
-      usedHiroshiCars: "Подержанные автомобили Hiroshi",
+    header: {
+      myCart: "моя тележка",
+      account: "аккаунт",
     },
-    rimsTires: {
-      title: "Обода и шины",
-      autobelloWheels: "Колеса Autobello",
-      etkWheels: "Колеса ETK",
-      folkWheels: "Колеса Folk",
-      gavrilWheels: "Колеса Gavril",
-      ibishuWheels: "Колеса Ibishu",
+    subHeader: {
+      usedVehicles: "Подержанные автомобили",
+      rimsTires: "Обода и шины",
+      careers: "Карьера",
+      helpCenter: "Центр помощи",
+      about: "предложение",
     },
-    careers: {
-      title: "Карьера",
-      openPositions: "Открытые позиции",
-      internshipProgram: "Программа стажировки",
-      carJockeyRequirements: "Требования к жокею автомобиля",
-    },
-    helpCenter: {
-      title: "Центр помощи",
-      questionAnswers: "вопросы и ответы",
-      delivery: "Доставка",
-      contact: "Связаться с",
-    },
-    about: {
-      title: "о нашей компании",
-      ourCompany: "Наша компания",
-      whereToFindUs: "Где нас найти",
+    navigation: {
+      home: "дом",
+      usedVehicles: {
+        title: "Подержанные автомобили",
+        usedCivettaCars: "Подержанные автомобили Civetta",
+        usedIbishuCars: "Подержанные автомобили Ibishu",
+        usedGavrilCars: "Подержанные автомобили Gavril",
+        usedEtkCars: "Подержанные автомобили ETK",
+        usedHiroshiCars: "Подержанные автомобили Hiroshi",
+      },
+      rimsTires: {
+        title: "Обода и шины",
+        autobelloWheels: "Колеса Autobello",
+        etkWheels: "Колеса ETK",
+        folkWheels: "Колеса Folk",
+        gavrilWheels: "Колеса Gavril",
+        ibishuWheels: "Колеса Ibishu",
+      },
+      careers: {
+        title: "Карьера",
+        openPositions: "Открытые позиции",
+        internshipProgram: "Программа стажировки",
+        carJockeyRequirements: "Требования к жокею автомобиля",
+      },
+      helpCenter: {
+        title: "Центр помощи",
+        questionAnswers: "вопросы и ответы",
+        delivery: "Доставка",
+        contact: "Связаться с",
+      },
+      about: {
+        title: "о нашей компании",
+        ourCompany: "Наша компания",
+        whereToFindUs: "Где нас найти",
+      },
     },
   },
 };

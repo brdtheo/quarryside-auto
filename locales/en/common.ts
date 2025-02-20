9f9627e9216bba5a7a4dee4156739d9de8cb8f8a
Theo Billardey <toto.billardey@gmail.com>
2025-02-20 19:09:25 +0100

    chore(i18n): add namespace key in JSON files

--- locales/en/common.ts
+++ locales/en/common.ts
@@ -1,66 +1,68 @@
 export default {
-  notFoundTitle: "Not found",
-  notFoundDescription:
-    "It looks like the content you're looking for does not exist or has been moved. Enjoy this bouncing Covet instead:",
-  reviews: "Reviews",
-  search: "Search",
-  resultCount: "{{ count }} result(s)",
-  sortResults: {
-    title: "Sort results",
-    option: {
-      priceAsc: "Price - low to high",
-      priceDesc: "Price - high to low",
-      mileageAsc: "Mileage - low to high",
-      mileageDesc: "Mileage - high to low",
-      yearAsc: "Year - oldest first",
-      yearDesc: "Year - most recent first",
+  common: {
+    notFoundTitle: "Not found",
+    notFoundDescription:
+      "It looks like the content you're looking for does not exist or has been moved. Enjoy this bouncing Covet instead:",
+    reviews: "Reviews",
+    search: "Search",
+    resultCount: "{ count } result(s)",
+    sortResults: {
+      title: "Sort results",
+      option: {
+        priceAsc: "Price - low to high",
+        priceDesc: "Price - high to low",
+        mileageAsc: "Mileage - low to high",
+        mileageDesc: "Mileage - high to low",
+        yearAsc: "Year - oldest first",
+        yearDesc: "Year - most recent first",
+      },
     },
-  },
-  header: {
-    myCart: "My Cart",
-    account: "Account",
-  },
-  subHeader: {
-    usedVehicles: "Used vehicles",
-    rimsTires: "Rims & tires",
-    careers: "Careers",
-    helpCenter: "Help center",
-    about: "About",
-  },
-  navigation: {
-    home: "Home",
-    usedVehicles: {
-      title: "Used vehicles",
-      usedCivettaCars: "Used Civetta cars",
-      usedIbishuCars: "Used Ibishu cars",
-      usedGavrilCars: "Used Gavril cars",
-      usedEtkCars: "Used ETK cars",
-      usedHiroshiCars: "Used Hiroshi cars",
+    header: {
+      myCart: "My Cart",
+      account: "Account",
     },
-    rimsTires: {
-      title: "Rims & tires",
-      autobelloWheels: "Autobello wheels",
-      etkWheels: "ETK wheels",
-      folkWheels: "Folk wheels",
-      gavrilWheels: "Gavril wheels",
-      ibishuWheels: "Ibishu wheels",
+    subHeader: {
+      usedVehicles: "Used vehicles",
+      rimsTires: "Rims & tires",
+      careers: "Careers",
+      helpCenter: "Help center",
+      about: "About",
     },
-    careers: {
-      title: "Careers",
-      openPositions: "Open positions",
-      internshipProgram: "Internship program",
-      carJockeyRequirements: "Car jockey requirements",
-    },
-    helpCenter: {
-      title: "Help Center",
-      questionAnswers: "Q&As",
-      delivery: "Delivery",
-      contact: "Contact",
-    },
-    about: {
-      title: "About",
-      ourCompany: "Our company",
-      whereToFindUs: "Where to find us",
+    navigation: {
+      home: "Home",
+      usedVehicles: {
+        title: "Used vehicles",
+        usedCivettaCars: "Used Civetta cars",
+        usedIbishuCars: "Used Ibishu cars",
+        usedGavrilCars: "Used Gavril cars",
+        usedEtkCars: "Used ETK cars",
+        usedHiroshiCars: "Used Hiroshi cars",
+      },
+      rimsTires: {
+        title: "Rims & tires",
+        autobelloWheels: "Autobello wheels",
+        etkWheels: "ETK wheels",
+        folkWheels: "Folk wheels",
+        gavrilWheels: "Gavril wheels",
+        ibishuWheels: "Ibishu wheels",
+      },
+      careers: {
+        title: "Careers",
+        openPositions: "Open positions",
+        internshipProgram: "Internship program",
+        carJockeyRequirements: "Car jockey requirements",
+      },
+      helpCenter: {
+        title: "Help Center",
+        questionAnswers: "Q&As",
+        delivery: "Delivery",
+        contact: "Contact",
+      },
+      about: {
+        title: "About",
+        ourCompany: "Our company",
+        whereToFindUs: "Where to find us",
+      },
     },
   },
 };

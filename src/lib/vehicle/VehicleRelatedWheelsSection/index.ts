cc6d55b9ac02a6a13bbcbc6853aae6cb0ecd219c
Theo Billardey <toto.billardey@gmail.com>
2025-02-22 19:52:25 +0100

    feat(vehicle): create related wheels section

---
+++ src/lib/vehicle/VehicleRelatedWheelsSection/index.ts
@@ -0,0 +1,5 @@
+import VehicleRelatedWheelsSection from "./VehicleRelatedWheelsSection";
+import type { VehicleRelatedWheelsSectionProps } from "./vehiclerelatedwheels";
+
+export type { VehicleRelatedWheelsSectionProps };
+export default VehicleRelatedWheelsSection;

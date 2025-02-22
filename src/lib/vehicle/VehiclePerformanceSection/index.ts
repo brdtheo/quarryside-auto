bb46e1289b6aff6c04646e7c4e6b13c537410cb3
Theo Billardey <toto.billardey@gmail.com>
2025-02-22 19:51:39 +0100

    feat(vehicle): create performance section

---
+++ src/lib/vehicle/VehiclePerformanceSection/index.ts
@@ -0,0 +1,5 @@
+import VehiclePerformanceSection from "./VehiclePerformanceSection";
+import type { VehiclePerformanceSectionProps } from "./vehicleperformancesection";
+
+export type { VehiclePerformanceSectionProps };
+export default VehiclePerformanceSection;

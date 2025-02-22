1e28e6f5d97286647fe374ca6df2f2cc3c2a6dd4
Theo Billardey <toto.billardey@gmail.com>
2025-02-22 19:52:40 +0100

    feat(vehicle): create vehicle form

---
+++ src/lib/vehicle/VehicleForm/VehicleForm.tsx
@@ -0,0 +1,56 @@
+import { getTranslations } from "next-intl/server";
+
+import Button from "@/components/Button";
+import TextField from "@/components/TextField";
+
+export default async function VehicleForm() {
+  const t = await getTranslations("vehicles");
+
+  return (
+    <form
+      action="#"
+      noValidate
+      className="bg-background dark:bg-black rounded py-8 px-4 flex flex-col gap-6"
+    >
+      <h2 className="font-semibold text-base">{t("form.title")}</h2>
+
+      <div className="grid grid-cols-2 gap-2">
+        <TextField
+          className="col-span-2 @md/vehicleform:col-span-1 w-full"
+          placeholder={t("form.field.firstName")}
+          value=""
+        />
+        <TextField
+          className="col-span-2 @md/vehicleform:col-span-1 w-full"
+          placeholder={t("form.field.lastName")}
+          value=""
+        />
+        <TextField
+          className="col-span-2 @md/vehicleform:col-span-1 w-full"
+          placeholder={t("form.field.email")}
+          value=""
+        />
+        <TextField
+          className="col-span-2 @md/vehicleform:col-span-1 w-full"
+          placeholder={t("form.field.phone")}
+          value=""
+        />
+        <TextField
+          className="col-span-2 h-24"
+          placeholder={t("form.field.message")}
+          isTextArea
+          value=""
+        />
+      </div>
+
+      <div className="flex flex-col gap-2">
+        <Button color="primary" size="lg" rounded>
+          {t("checkAvailability")}
+        </Button>
+        <Button color="secondary" rounded>
+          {t("scheduleTestDrive")}
+        </Button>
+      </div>
+    </form>
+  );
+}

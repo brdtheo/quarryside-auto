515da81af5eedd3c46f8db367acca79da980d7e9
Theo Billardey <toto.billardey@gmail.com>
2025-02-25 19:14:09 +0100

    feat(components): create home hero

---
+++ locales/en/home.ts
@@ -0,0 +1,12 @@
+export default {
+  home: {
+    hero: {
+      title:
+        "Shop hundreds of used vehicles and auto parts in Quarryside county",
+      subtitle:
+        "Supported by our strong driver community across the West Coast",
+      browseUsedCars: "Browse used cars",
+      browseRimsTires: "Browse rims and tires",
+    },
+  },
+};

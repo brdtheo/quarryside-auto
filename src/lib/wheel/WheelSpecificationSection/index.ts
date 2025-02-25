65f8210e1ccd4abee901b2b2f8f475f78ffe2267
Theo Billardey <toto.billardey@gmail.com>
2025-02-25 14:35:30 +0100

    feat(wheel): create specification section

---
+++ src/lib/wheel/WheelSpecificationSection/index.ts
@@ -0,0 +1,5 @@
+import WheelSpecificationSection from "./WheelSpecificationSection";
+import type { WheelSpecificationSectionProps } from "./wheelpecificationsection";
+
+export type { WheelSpecificationSectionProps };
+export default WheelSpecificationSection;

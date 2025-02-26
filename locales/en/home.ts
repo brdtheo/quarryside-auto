f0eea1e00256b90d71b92b40c11ca97f5b0cd0f5
Theo Billardey <toto.billardey@gmail.com>
2025-02-26 15:56:17 +0100

    feat(components): create client feedback list

--- locales/en/home.ts
+++ locales/en/home.ts
@@ -8,5 +8,26 @@ export default {
       browseUsedCars: "Browse used cars",
       browseRimsTires: "Browse rims and tires",
     },
+    feedback: {
+      title: "Feeback from our clients",
+      0: {
+        quote:
+          "The car jockey took extra care and warmed the tires before reaching to me. My drift car has been delivered in front of my house only 1 hour after I bought it!",
+        author: "M. Mouton",
+        date: "3 years ago",
+      },
+      1: {
+        quote:
+          "My D15 is perfectly working and there arent any scratches! The driver is really fast! I would recommend",
+        author: "B. Aguette",
+        date: "6 months ago",
+      },
+      2: {
+        quote:
+          "They said nobody could drive my Pigeon directly so they used a truck to deliver it to my house. Plus I got the stabilizers gratis.",
+        author: "C. Roissant",
+        date: "2 years ago",
+      },
+    },
   },
 };
--- messages/en.ts
+++ messages/en.ts
@@ -1,5 +1,6 @@
 import common from "../locales/en/common";
+import home from "../locales/en/home";
 import vehicles from "../locales/en/vehicles";
 import wheels from "../locales/en/wheels";
 
-export default { ...common, ...vehicles, ...wheels };
+export default { ...common, ...vehicles, ...wheels, ...home };

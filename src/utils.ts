40471ff5ec769aef89747f27880a9b2ff7251af8
Theo Billardey <toto.billardey@gmail.com>
2025-02-26 18:00:36 +0100

    feat(utils): create prisma args getter
    Create a util Fn to get prisma find many args in home page

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,5 @@
+import { Prisma } from "@prisma/client";
+
 import currency from "currency.js";
 
 /**
@@ -11,3 +13,24 @@ export const getPrice = (priceCts: bigint, options?: currency.Options) => {
   if (!priceCts) return "";
   return currency(Number(priceCts) / 100, currencyOptions).format();
 };
+
+/**
+ * Returns the complete list of Prisma args to pass for Home page
+ * @returns {VehicleFindManyArgs}
+ */
+export function getHomeFindManyArgs(): Prisma.VehicleFindManyArgs {
+  return {
+    where: {
+      slug: {
+        in: [
+          "gavril-d-series-d15-v8-4wd",
+          "gavril-roamer-v8-automatic",
+          "soliad-lansdale-2-5-facelift",
+        ],
+      },
+    },
+    include: {
+      medias: { where: { is_thumbnail: true } },
+    },
+  };
+}

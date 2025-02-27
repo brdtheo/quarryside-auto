eb74ea273c155ec97d5337eb58157e39aa7a75e2
Theo Billardey <toto.billardey@gmail.com>
2025-02-27 16:10:54 +0100

    feat(components): set proper hierarchy for heading elements

--- src/components/HeaderSideDrawerListItem/HeaderSideDrawerListItem.tsx
+++ src/components/HeaderSideDrawerListItem/HeaderSideDrawerListItem.tsx
@@ -12,7 +12,7 @@ export default function HeaderSideDrawerListItem({
     <li className="border-b border-b-divider last-of-type:border-b-transparent px-2 py-3">
       <Link onClick={onClick} className="flex items-center gap-3" href={href}>
         {icon}
-        <h5 className="text-sm">{children}</h5>
+        <h3 className="text-sm">{children}</h3>
       </Link>
     </li>
   );

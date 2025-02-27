dfaeccd22938ca22347aa7481e09f7198b36d456
Theo Billardey <toto.billardey@gmail.com>
2025-02-27 16:10:30 +0100

    feat(components): add missing li elements for lists

--- src/components/ClientFeedbackList/ClientFeedbackList.tsx
+++ src/components/ClientFeedbackList/ClientFeedbackList.tsx
@@ -8,24 +8,30 @@ export default async function ClientFeedbackList() {
   return (
     <HomeSection title={t("feedback.title")}>
       <ul className="grid grid-cols-1 gap-4 @md/homecontainer:grid-cols-2 @3xl/homecontainer:grid-cols-3">
-        <ClientFeedback
-          author={t("feedback.0.author")}
-          date={t("feedback.0.date")}
-        >
-          {t("feedback.0.quote")}
-        </ClientFeedback>
-        <ClientFeedback
-          author={t("feedback.1.author")}
-          date={t("feedback.1.date")}
-        >
-          {t("feedback.1.quote")}
-        </ClientFeedback>
-        <ClientFeedback
-          author={t("feedback.2.author")}
-          date={t("feedback.2.date")}
-        >
-          {t("feedback.2.quote")}
-        </ClientFeedback>
+        <li>
+          <ClientFeedback
+            author={t("feedback.0.author")}
+            date={t("feedback.0.date")}
+          >
+            {t("feedback.0.quote")}
+          </ClientFeedback>
+        </li>
+        <li>
+          <ClientFeedback
+            author={t("feedback.1.author")}
+            date={t("feedback.1.date")}
+          >
+            {t("feedback.1.quote")}
+          </ClientFeedback>
+        </li>
+        <li>
+          <ClientFeedback
+            author={t("feedback.2.author")}
+            date={t("feedback.2.date")}
+          >
+            {t("feedback.2.quote")}
+          </ClientFeedback>
+        </li>
       </ul>
     </HomeSection>
   );

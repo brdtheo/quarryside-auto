7a255c678b8f63d72e2ef8b40d647966322ea641
Theo Billardey <toto.billardey@gmail.com>
2025-02-27 17:12:59 +0100

    build(sentry): add package
    Add Sentry with base configuration

---
+++ sentry.client.config.ts
@@ -0,0 +1,14 @@
+// This file configures the initialization of Sentry on the client.
+// The config you add here will be used whenever a users loads a page in their browser.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://fc7b798657e81f67183e7c28127be29d@o1075246.ingest.us.sentry.io/4508891998846976",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});

effb5b819765fce31db3df45594b766d43fdca2f
Theo Billardey <toto.billardey@gmail.com>
2025-03-01 23:27:49 +0100

    refactor(components): optimize advertising
    Leverage Image component from Next for better performance

--- src/components/Advertising/index.ts
+++ src/components/Advertising/index.ts
@@ -2,14 +2,9 @@ import Advertising from "./Advertising";
 import type { AdvertisingProps, AdvertisingRatioMode } from "./advertising";
 import {
   ADVERTISING_BASE_URL,
-  ADVERTISING_RATIO_MODE_LIST,
   MAX_IMAGE_COUNT_PER_RATIO_MODE_MAPPER,
 } from "./constants";
 
-export {
-  ADVERTISING_RATIO_MODE_LIST,
-  ADVERTISING_BASE_URL,
-  MAX_IMAGE_COUNT_PER_RATIO_MODE_MAPPER,
-};
+export { ADVERTISING_BASE_URL, MAX_IMAGE_COUNT_PER_RATIO_MODE_MAPPER };
 export type { AdvertisingProps, AdvertisingRatioMode };
 export default Advertising;

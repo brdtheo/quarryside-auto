3804ad2efe684c51f0255f073d10c7a8620e3a26
Theo Billardey <toto.billardey@gmail.com>
2025-03-04 16:48:31 +0100

    feat(components): update side drawer display
    Update side drawer props and display for desktop

--- src/components/SideDrawer/SideDrawer.tsx
+++ src/components/SideDrawer/SideDrawer.tsx
@@ -14,10 +14,10 @@ export default function SideDrawer({
       <div className="w-dvw h-dvh fixed top-0 left-0 z-30">
         <button
           role="button"
-          className="bg-black opacity-80 w-dvw h-dvh"
+          className="bg-black/80 w-dvw h-dvh"
           onClick={onClose}
         />
-        <div className="w-60 bg-background dark:bg-backgrounddark border-l border-l-divider dark:border-l-dividerdark fixed top-0 right-0 h-dvh px-4 py-6 gap-6 flex flex-col overflow-y-scroll">
+        <div className="w-60 md:w-80 bg-background dark:bg-backgrounddark border-l border-l-divider dark:border-l-dividerdark fixed top-0 right-0 h-dvh px-4 py-6 gap-6 flex flex-col overflow-y-auto">
           <IconButton className="border border-divider" onClick={onClose}>
             <IconX size={20} stroke={2} />
           </IconButton>
--- src/components/SideDrawer/sidedrawer.d.ts
+++ src/components/SideDrawer/sidedrawer.d.ts
@@ -1,7 +1,7 @@
 import { ReactElement } from "react";
 
 export type SideDrawerProps = {
-  children: ReactElement;
+  children: ReactElement[] | ReactElement;
   isOpen: boolean;
   onClose: () => void;
 };

801e46d302a552803366e322d124020533ea642b
Theo Billardey <toto.billardey@gmail.com>
2025-03-07 17:13:42 +0100

    feat(components): hide list filter header search and sort

--- src/components/ListFilterHeader/ListFilterHeader.tsx
+++ src/components/ListFilterHeader/ListFilterHeader.tsx
@@ -9,14 +9,10 @@ import {
 } from "@tabler/icons-react";
 
 import IconButton from "@/components/IconButton";
-import SearchField from "@/components/SearchField";
-import Select from "@/components/Select";
 
 import type { ListFilterHeaderProps } from ".";
 
 export default function ListFilterHeader({
-  textSearch,
-  sortOptionList,
   resultCount = 0,
   activeFilterCount,
   handleOpenFilterDrawer,
@@ -25,15 +21,6 @@ export default function ListFilterHeader({
 
   return (
     <>
-      <div className="hidden md:flex justify-between pb-6 gap-4">
-        <SearchField isDisabled value={textSearch} onChange={() => {}} />
-        <Select
-          options={sortOptionList}
-          placeholder={t("sortResults.title")}
-          value=""
-          onChange={() => {}}
-        />
-      </div>
       <div className="flex md:hidden justify-between pb-6 gap-4">
         <IconButton
           isDisabled

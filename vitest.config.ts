787bf85d7144e748252dcca8a77538e088d52dd9
Theo Billardey <toto.billardey@gmail.com>
2025-03-08 15:57:16 +0100

    build(CI): add vitest to static analysis

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from "vitest/config";
 
-import tsconfigPaths from "vite-tsconfig-paths";
+import tsconfigPaths from "./node_modules/vite-tsconfig-paths/dist/index.js";
 
 export default defineConfig({
   test: {

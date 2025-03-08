e5e96e7c06141f7c1e07192809274db151975103
Theo Billardey <toto.billardey@gmail.com>
2025-03-08 12:59:46 +0100

    chore(prettier): run CLI

--- src/components/BenefitCard/BenefitCard.tsx
+++ src/components/BenefitCard/BenefitCard.tsx
@@ -8,7 +8,9 @@ export default async function BenefitCard({
   return (
     <article>
       <div className="px-6 py-4 rounded flex flex-col bg-secondary/40">
-        <div className="text-primary/90 dark:text-primarydark/90 flex justify-center">{icon}</div>
+        <div className="text-primary/90 dark:text-primarydark/90 flex justify-center">
+          {icon}
+        </div>
         <h2 className="text-primary dark:text-primarydark text-center text-lg font-semibold">
           {title}
         </h2>

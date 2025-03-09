ee7711f4e1b0637782f36586e271d348597d8a31
Theo Billardey <toto.billardey@gmail.com>
2025-03-09 13:43:02 +0100

    config(vitest): set environment and import react plugin

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,12 +1,15 @@
 import { defineConfig } from "vitest/config";
 
+import react from "@vitejs/plugin-react";
+
 import tsconfigPaths from "./node_modules/vite-tsconfig-paths/dist/index.js";
 
 export default defineConfig({
   test: {
+    environment: "happy-dom",
     coverage: {
       provider: "v8",
     },
   },
-  plugins: [tsconfigPaths()],
+  plugins: [tsconfigPaths(), react()],
 });

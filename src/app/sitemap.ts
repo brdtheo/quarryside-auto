ab5ba80b3a7d8de0d20838ff13eb3423fdfbe072
Theo Billardey <toto.billardey@gmail.com>
2025-03-12 16:53:55 +0100

    fix(next): update localized sitemap URLs
    Set URL to default locale and keep alternate for other available locales

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -9,58 +9,53 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const wheels = await prisma.wheel.findMany({ select: { slug: true } });
 
   const vehiclesMapped = vehicles.map((vehicle) => ({
-    url: `${DOMAIN_URL}/vehicles/${vehicle.slug}`,
+    url: `${DOMAIN_URL}/en/vehicles/${vehicle.slug}`,
     alternates: {
       languages: {
-        en: `${DOMAIN_URL}/en/vehicles/${vehicle.slug}`,
         ru: `${DOMAIN_URL}/ru/vehicles/${vehicle.slug}`,
       },
     },
     lastModified: new Date(),
-    priority: 0.8,
+    priority: 1,
   }));
   const wheelsMapped = wheels.map((wheel) => ({
-    url: `${DOMAIN_URL}/wheels/${wheel.slug}`,
+    url: `${DOMAIN_URL}/en/wheels/${wheel.slug}`,
     alternates: {
       languages: {
-        en: `${DOMAIN_URL}/en/wheels/${wheel.slug}`,
         ru: `${DOMAIN_URL}/ru/wheels/${wheel.slug}`,
       },
     },
     lastModified: new Date(),
-    priority: 0.8,
+    priority: 1,
   }));
 
   return [
     {
-      url: DOMAIN_URL,
+      url: `${DOMAIN_URL}/en`,
       lastModified: new Date(),
       priority: 1,
       alternates: {
         languages: {
-          en: `${DOMAIN_URL}/en`,
           ru: `${DOMAIN_URL}/ru`,
         },
       },
     },
     {
-      url: `${DOMAIN_URL}/vehicles`,
+      url: `${DOMAIN_URL}/en/vehicles`,
       lastModified: new Date(),
       priority: 1,
       alternates: {
         languages: {
-          en: `${DOMAIN_URL}/en/vehicles`,
           ru: `${DOMAIN_URL}/ru/vehicles`,
         },
       },
     },
     {
-      url: `${DOMAIN_URL}/wheels`,
+      url: `${DOMAIN_URL}/en/wheels`,
       lastModified: new Date(),
       priority: 1,
       alternates: {
         languages: {
-          en: `${DOMAIN_URL}/en/wheels`,
           ru: `${DOMAIN_URL}/ru/wheels`,
         },
       },

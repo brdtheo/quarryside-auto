c359735ebf1e1da881baf132e6fe5839f4c2502e
Theo Billardey <toto.billardey@gmail.com>
2025-03-14 21:01:25 +0100

    test(vehicle): create base tests

---
+++ src/lib/vehicle/VehicleList/vehiclelist.test.tsx
@@ -0,0 +1,44 @@
+import { cleanup, render, screen } from "@testing-library/react";
+
+import { afterEach, describe, expect, it } from "vitest";
+
+import { vehicleListFactory } from "@/lib/vehicle/factory";
+import { VehicleWithMedias } from "@/lib/vehicle/types";
+
+import VehicleList from ".";
+
+describe("VehicleList", () => {
+  const vehicles = vehicleListFactory(5, {
+    withMedia: true,
+  }) as VehicleWithMedias[];
+
+  afterEach(() => {
+    cleanup();
+  });
+
+  it("Renders a list element", () => {
+    render(<VehicleList data={vehicles} itemRender={() => {}} />);
+    const list = screen.getByRole("list");
+    expect(list).toBeInTheDocument();
+  });
+
+  it("Renders the item render elements", () => {
+    render(
+      <VehicleList
+        data={vehicles}
+        itemRender={(vehicle) => (
+          <li key={vehicle.id}>
+            <h1>{vehicle.model}</h1>
+            <p>{vehicle.description}</p>
+          </li>
+        )}
+      />,
+    );
+    const listItems = screen.getAllByRole("listitem");
+    const headings = screen.getAllByRole("heading");
+    const paragraphs = screen.getAllByRole("paragraph");
+    expect(listItems).toHaveLength(5);
+    expect(headings).toHaveLength(5);
+    expect(paragraphs).toHaveLength(5);
+  });
+});
---
+++ src/lib/vehicle/hooks/useVehicleDetails.test.ts
@@ -0,0 +1,192 @@
+import { getTranslations } from "next-intl/server";
+
+import { renderHook } from "@testing-library/react-hooks/server";
+
+import { describe, expect, expectTypeOf, it } from "vitest";
+
+import { vehicleFactory } from "@/lib/vehicle/factory";
+import useVehicleDetails from "@/lib/vehicle/hooks/useVehicleDetails";
+import { getMonthlyEstimatePrice } from "@/lib/vehicle/utils";
+
+import { getPrice } from "@/utils";
+
+describe("useVehicleDetails", async () => {
+  const t = await getTranslations("vehicles");
+  const vehicle = vehicleFactory();
+  const {
+    result: { current },
+  } = renderHook(async () => await useVehicleDetails(vehicle));
+  const vehicleDetails = await current;
+
+  it("Returns an object with empty values if param is empty", async () => {
+    const {
+      result: { current },
+      // @ts-expect-error We simulate an unexpected behavior
+    } = renderHook(async () => await useVehicleDetails(undefined));
+    const result = await current;
+    expect(result).toBeDefined();
+    Object.entries(current).forEach((entry) => {
+      const key = entry[0];
+      const value = entry[1];
+      expect(key).toBeTruthy();
+      expect(value).toBe("");
+    });
+  });
+
+  it("Returns the vehicle's body style", () => {
+    expectTypeOf(vehicleDetails.bodyStyle).toEqualTypeOf<string>();
+    expect(vehicleDetails.bodyStyle).toBeTruthy();
+    expect(vehicleDetails.bodyStyle).toBe(
+      t(`filter.body_style.option.${vehicle.body_style}`),
+    );
+  });
+
+  it("Returns the vehicle's brand", () => {
+    expectTypeOf(vehicleDetails.brand).toEqualTypeOf<string>();
+    expect(vehicleDetails.brand).toBeTruthy();
+    expect(vehicleDetails.brand).toBe(
+      t(`filter.brand.option.${vehicle.brand}`),
+    );
+  });
+
+  it("Returns the vehicle's condition", () => {
+    expectTypeOf(vehicleDetails.condition).toEqualTypeOf<string>();
+    expect(vehicleDetails.condition).toBeTruthy();
+    expect(vehicleDetails.condition).toBe(
+      t(`filter.condition.option.${vehicle.condition}`),
+    );
+  });
+
+  it("Returns the vehicle's country", () => {
+    expectTypeOf(vehicleDetails.country).toEqualTypeOf<string>();
+    expect(vehicleDetails.country).toBeTruthy();
+    expect(vehicleDetails.country).toBe(
+      t(`filter.country.option.${vehicle.country}`),
+    );
+  });
+
+  it("Returns the vehicle's drivetrain", () => {
+    expectTypeOf(vehicleDetails.drivetrain).toEqualTypeOf<string>();
+    expect(vehicleDetails.drivetrain).toBeTruthy();
+    expect(vehicleDetails.drivetrain).toBe(
+      t(`filter.drivetrain.option.${vehicle.drivetrain}`),
+    );
+  });
+
+  it("Returns the vehicle's engine", () => {
+    expectTypeOf(vehicleDetails.engine).toEqualTypeOf<string>();
+    if (vehicle.engine_cylinder_count) {
+      expect(vehicleDetails.engine).toBe(
+        t("details.engine.value", {
+          engineDisplacementVolumeLiters: Number(
+            vehicle.engine_displacement_volume_liters,
+          ),
+          engineLayout: vehicle.engine_layout,
+          engineCylinderCount: vehicle.engine_cylinder_count,
+        }),
+      );
+    } else {
+      expect(vehicleDetails.engine).toBe("");
+    }
+  });
+
+  it("Returns the vehicle's link href", () => {
+    expectTypeOf(vehicleDetails.href).toEqualTypeOf<string>();
+    expect(vehicleDetails.href).toBeTruthy();
+    expect(vehicleDetails.href).toBe(
+      `/vehicles/${encodeURIComponent(vehicle.slug)}`,
+    );
+  });
+
+  it("Returns the vehicle's mileage", () => {
+    expectTypeOf(vehicleDetails.mileage).toEqualTypeOf<string>();
+    expect(vehicleDetails.mileage).toBeTruthy();
+    expect(vehicleDetails.mileage).toBe(
+      t("details.mileage.value", {
+        mileage: vehicle.mileage,
+      }),
+    );
+  });
+
+  it("Returns the vehicle's monthly estimate price", () => {
+    expectTypeOf(vehicleDetails.monthlyEstimatePrice).toEqualTypeOf<string>();
+    expect(vehicleDetails.monthlyEstimatePrice).toBeTruthy();
+    expect(vehicleDetails.monthlyEstimatePrice).toBe(
+      getMonthlyEstimatePrice(vehicle.price_cts),
+    );
+  });
+
+  it("Returns the vehicle's power", () => {
+    expectTypeOf(vehicleDetails.power).toEqualTypeOf<string>();
+    expect(vehicleDetails.power).toBeTruthy();
+    expect(vehicleDetails.power).toBe(
+      t("details.power.value", {
+        power: Number(vehicle.power_bhp),
+      }),
+    );
+  });
+
+  it("Returns the vehicle's price", () => {
+    expectTypeOf(vehicleDetails.price).toEqualTypeOf<string>();
+    expect(vehicleDetails.price).toBeTruthy();
+    expect(vehicleDetails.price).toBe(getPrice(vehicle.price_cts));
+  });
+
+  it("Returns the vehicle's title", () => {
+    expectTypeOf(vehicleDetails.title).toEqualTypeOf<string>();
+    expect(vehicleDetails.title).toBeTruthy();
+    expect(vehicleDetails.title).toBe(
+      [
+        vehicle.year,
+        t(`filter.brand.option.${vehicle.brand}`),
+        vehicle.model,
+      ].join(" "),
+    );
+  });
+
+  it("Returns the vehicle's title without year", () => {
+    expectTypeOf(vehicleDetails.titleWithoutYear).toEqualTypeOf<string>();
+    expect(vehicleDetails.titleWithoutYear).toBeTruthy();
+    expect(vehicleDetails.titleWithoutYear).toBe(
+      [t(`filter.brand.option.${vehicle.brand}`), vehicle.model].join(" "),
+    );
+  });
+
+  it("Returns the vehicle's top speed", () => {
+    expectTypeOf(vehicleDetails.topSpeed).toEqualTypeOf<string>();
+    expect(vehicleDetails.topSpeed).toBeTruthy();
+    expect(vehicleDetails.topSpeed).toBe(
+      t("details.topSpeed.value", {
+        speed: vehicle.top_speed_mph,
+      }),
+    );
+  });
+
+  it("Returns the vehicle's transmission", () => {
+    expectTypeOf(vehicleDetails.transmission).toEqualTypeOf<string>();
+    expect(vehicleDetails.transmission).toBeTruthy();
+    expect(vehicleDetails.transmission).toBe(
+      t(`filter.transmission.option.${vehicle.transmission}`),
+    );
+  });
+
+  it("Returns the vehicle's weight", () => {
+    expectTypeOf(vehicleDetails.weight).toEqualTypeOf<string>();
+    expect(vehicleDetails.weight).toBeTruthy();
+    expect(vehicleDetails.weight).toBe(
+      t("details.weight.value", {
+        weight: Number(vehicle.weight_lbs),
+      }),
+    );
+  });
+
+  it("Returns the vehicle's 0 to 60 seconds", () => {
+    expectTypeOf(vehicleDetails.zeroToSixtySeconds).toEqualTypeOf<string>();
+    expect(vehicleDetails.zeroToSixtySeconds).toBeTruthy();
+    expect(vehicleDetails.zeroToSixtySeconds).toBe(
+      t("details.zeroToSixty.value", {
+        seconds: Number(vehicle.zero_to_sixty_seconds),
+      }),
+    );
+  });
+});

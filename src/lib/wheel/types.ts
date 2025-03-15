89e9246853efa8a81dc4677468c60f9c7710f499
Theo Billardey <toto.billardey@gmail.com>
2025-03-15 20:34:45 +0100

    feat(wheel): provide strucured data

--- src/lib/wheel/types.ts
+++ src/lib/wheel/types.ts
@@ -1,3 +1,11 @@
 import { Media, Wheel } from "@prisma/client";
 
 export type WheelWithMedias = Wheel & { medias: Media[] };
+
+export type WheelRichDataParams = {
+  brand: string;
+  thumbnail: string;
+  name: string;
+  price: string;
+  slug: string;
+};

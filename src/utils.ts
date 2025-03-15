dba083b2f71152ce2507a358caa7316dfaf34270
Theo Billardey <toto.billardey@gmail.com>
2025-03-15 14:51:02 +0100

    fix(utils): update getprice util param typing

--- src/utils.ts
+++ src/utils.ts
@@ -8,7 +8,10 @@ import currency from "currency.js";
  * @param options
  * @returns {string}
  */
-export const getPrice = (priceCts: bigint, options?: currency.Options) => {
+export const getPrice = (
+  priceCts: bigint | null,
+  options?: currency.Options,
+) => {
   const currencyOptions = options ?? { precision: 0 };
   if (!priceCts) return "";
   return currency(Number(priceCts) / 100, currencyOptions).format();

5f19acf602838825477bb0bef8b86d88bc001855
Theo Billardey <toto.billardey@gmail.com>
2025-03-15 22:00:44 +0100

    feat(blog): better semantic for date and time infos

--- src/lib/review/ReviewCard/ReviewCard.tsx
+++ src/lib/review/ReviewCard/ReviewCard.tsx
@@ -15,7 +15,7 @@ export default function ReviewCard({
     <article className="bg-white border border-grey rounded p-4 flex flex-col gap-3">
       <div className="flex justify-between">
         <span className="font-medium text-sm">{title}</span>
-        <span className="text-sm">{date}</span>
+        <time className="text-sm">{date}</time>
       </div>
       <Rating size={14} score={rating} />
       <p className="text-sm">{description}</p>

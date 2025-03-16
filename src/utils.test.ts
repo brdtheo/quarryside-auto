0cc603247a4d093c19352c37503c125b790e2d0e
Theo Billardey <toto.billardey@gmail.com>
2025-03-16 14:34:09 +0100

    test(utils): provide describe step for global utils

--- src/utils.test.ts
+++ src/utils.test.ts
@@ -1,21 +1,23 @@
-import { expect, test } from "vitest";
+import { describe, expect, test } from "vitest";
 
 import { getHomeFindManyArgs } from "@/utils";
 
-test("retrieves prisma find many args for home page", () => {
-  const args = getHomeFindManyArgs();
-  expect(args).toStrictEqual({
-    where: {
-      slug: {
-        in: [
-          "gavril-d-series-d15-v8-4wd",
-          "gavril-roamer-v8-automatic",
-          "soliad-lansdale-2-5-facelift",
-        ],
+describe("Utils", () => {
+  test("Retrieves Prisma args for home page", () => {
+    const args = getHomeFindManyArgs();
+    expect(args).toStrictEqual({
+      where: {
+        slug: {
+          in: [
+            "gavril-d-series-d15-v8-4wd",
+            "gavril-roamer-v8-automatic",
+            "soliad-lansdale-2-5-facelift",
+          ],
+        },
       },
-    },
-    include: {
-      medias: { where: { is_thumbnail: true } },
-    },
+      include: {
+        medias: { where: { is_thumbnail: true } },
+      },
+    });
   });
 });

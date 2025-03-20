28cb4a27405bb6f2790201ea9f5c98589990603b
Theo Billardey <toto.billardey@gmail.com>
2025-03-20 15:23:58 +0100

    GCP config(storybook): add basic configuration

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -3,6 +3,8 @@ import type { Preview } from "@storybook/react";
 import "../src/app/globals.css";
 
 const preview: Preview = {
+    //👇 Enables auto-generated documentation for all stories
+  tags: ['autodocs'],
   parameters: {
     controls: {
       matchers: {

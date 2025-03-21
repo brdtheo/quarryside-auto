6042d77d1ddf45c2c0625a97b6cbe064c8aee02a
Theo Billardey <toto.billardey@gmail.com>
2025-03-21 20:48:53 +0100

    config(vitest): exclude playwright tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -19,6 +19,13 @@ export default defineConfig({
         inline: ["next-intl"],
       },
     },
+    exclude: [
+      "**/node_modules/**",
+      "**/dist/**",
+      "**/e2e/**",
+      "**/.{idea,git,cache,output,temp}/**",
+      "**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build,eslint,prettier}.config.*",
+    ],
   },
   plugins: [tsconfigPaths(), react()],
 });

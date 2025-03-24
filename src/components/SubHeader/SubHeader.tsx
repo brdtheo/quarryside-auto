461059033106fec452499c933f370eaebf055bcb
Theo Billardey <toto.billardey@gmail.com>
2025-03-24 18:12:11 +0100

    refactor(components): update subheader display
    Update subheader display and resolve accessibility issues

--- src/components/SubHeader/SubHeader.tsx
+++ src/components/SubHeader/SubHeader.tsx
@@ -7,8 +7,8 @@ export default function SubHeader() {
   const t = useTranslations("common");
 
   return (
-    <div className="bg-background dark:bg-black hidden md:flex justify-center h-10 border-b border-b-divider dark:border-y dark:border-y-dividerdark">
-      <Container className="flex justify-center items-center h-10">
+    <div className="bg-background brightness-98 dark:bg-black hidden md:flex justify-center h-11 border-b border-b-divider dark:border-y dark:border-y-dividerdark">
+      <Container className="flex justify-start items-center h-11">
         <nav className="inline-flex items-center h-8 gap-2">
           <SubHeaderLink slug="vehicles" label={t("subHeader.usedVehicles")} />
           <SubHeaderLink slug="wheels" label={t("subHeader.rimsTires")} />

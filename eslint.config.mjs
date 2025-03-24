38249836d95249af65b907db952a86b372dee469
Theo Billardey <toto.billardey@gmail.com>
2025-03-24 18:50:22 +0100

    config(eslint): import unicorn shared config
    Remove deprecated config file and import all rules from unicorn eslint plugin

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,20 @@
 import pluginNext from "@next/eslint-plugin-next";
 
-import pluginJs from "@eslint/js";
-import pluginReact from "eslint-plugin-react";
+import eslintPluginUnicorn from "eslint-plugin-unicorn";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
+  ...tseslint.configs.recommended,
+  eslintPluginUnicorn.configs.recommended,
+  {
+    rules: {
+      "unicorn/better-regex": "warn",
+    },
+  },
   { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
   { languageOptions: { globals: globals.browser } },
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
   { plugins: { "@next/next": pluginNext } },
   {
     rules: {

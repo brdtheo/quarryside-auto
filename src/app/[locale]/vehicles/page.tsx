d443164b3f1abee60ee7e1d19d54999b694ce0a3
Theo Billardey <toto.billardey@gmail.com>
2025-03-25 18:13:27 +0100

    feat(vehicle): separate data layer logic

--- src/app/[locale]/vehicles/page.tsx
+++ src/app/[locale]/vehicles/page.tsx
@@ -1,22 +1,19 @@
 import { Metadata } from "next";
 import { getTranslations } from "next-intl/server";
 
-import { Prisma } from "@prisma/client";
-
 import Container from "@/components/Container";
 import PageTitle from "@/components/PageTitle";
 import Pagination from "@/components/Pagination";
 
-import { prisma } from "@/lib/prisma";
 import VehicleCard from "@/lib/vehicle/VehicleCard";
 import VehicleList from "@/lib/vehicle/VehicleList";
 import VehicleListFilterAside from "@/lib/vehicle/VehicleListFilterAside";
 import VehicleListFilterHeader from "@/lib/vehicle/VehicleListFilterHeader";
 import { VEHICLE_LIST_PAGE_SIZE } from "@/lib/vehicle/constants";
-import { VehicleWithMedias } from "@/lib/vehicle/types";
-import { getVehicleFindManyArgs } from "@/lib/vehicle/utils";
 
 import { DOMAIN_URL, OPEN_GRAPH_IMAGE_URL } from "@/constants";
+
+import { getVehicleList, getVehicleListCount } from "@data/vehicle";
 
 import type { PageProps } from "@/types";
 
@@ -70,16 +67,8 @@ export default async function Page({ searchParams }: PageProps) {
   const params = await searchParams;
   const page = params?.page ? parseInt(params?.page as string, 10) : 1;
 
-  const prismaFindManyArgs = getVehicleFindManyArgs(params);
-  const prismaFindManyCountArgs = getVehicleFindManyArgs(params, true);
-
-  const vehicles = await prisma.vehicle.findMany(
-    prismaFindManyArgs as Prisma.VehicleFindManyArgs,
-  );
-  const vehiclesCount =
-    (await prisma.vehicle.count(
-      prismaFindManyCountArgs as Prisma.VehicleCountArgs,
-    )) ?? 0;
+  const vehicles = await getVehicleList(params);
+  const vehiclesCount = await getVehicleListCount(params);
 
   const t = await getTranslations("vehicles");
 
@@ -101,7 +90,7 @@ export default async function Page({ searchParams }: PageProps) {
           />
 
           <VehicleList
-            data={vehicles as VehicleWithMedias[]}
+            data={vehicles}
             itemRender={(vehicle) => (
               <li key={vehicle.id}>
                 <VehicleCard vehicle={vehicle} />

6a5b62cdf1889cbcebaa7538d84fe3d55f464e36
Theo Billardey <toto.billardey@gmail.com>
2025-03-28 18:12:57 +0100

    feat(components): update search field props
    Add clearable and search handler props for search field component

--- src/components/SearchField/SearchField.tsx
+++ src/components/SearchField/SearchField.tsx
@@ -1,10 +1,10 @@
 "use client";
 
-import { useCallback, useRef } from "react";
+import { KeyboardEvent, useCallback, useRef } from "react";
 
 import { useTranslations } from "next-intl";
 
-import { IconSearch } from "@tabler/icons-react";
+import { IconSearch, IconX } from "@tabler/icons-react";
 
 import clsx from "clsx";
 
@@ -14,7 +14,11 @@ export default function SearchField({
   className,
   value,
   isDisabled,
+  isClearable,
   onChange,
+  onSearch,
+  onClear,
+  ...props
 }: SearchFieldProps) {
   const t = useTranslations("common");
 
@@ -26,12 +30,34 @@ export default function SearchField({
     }
   }, []);
 
+  const handleKeyDown = useCallback(
+    (event: KeyboardEvent<HTMLInputElement>) => {
+      const pressedKey = event.key.toLowerCase();
+      const currentValue = inputRef?.current?.value;
+      if (
+        pressedKey === "enter" &&
+        !!inputRef &&
+        !!inputRef?.current &&
+        !!onSearch &&
+        !!currentValue &&
+        currentValue?.length >= 2
+      ) {
+        onSearch(currentValue);
+      }
+    },
+    [],
+  );
+
+  const handleClear = useCallback(() => {
+    onClear?.();
+  }, []);
+
   return (
     <div
       role="search"
       aria-disabled={isDisabled}
       className={clsx(
-        "flex border border-gray-300 dark:border-blacksecondary dark:hover:border-white rounded h-8 pl-3 w-fit transition-colors duration-75",
+        "flex border px-1 md:px-0 border-gray-300 dark:border-blacksecondary dark:hover:border-white rounded h-8 md:pr-3 w-fit transition-border duration-100 outline-2 outline-offset-1 outline-transparent focus-within:outline-primary dark:focus-within:outline-primarydark",
         {
           "bg-white dark:bg-transparent": !isDisabled,
           "disabled:bg-red-500 disabled:opacity-40": isDisabled,
@@ -39,15 +65,6 @@ export default function SearchField({
         className,
       )}
     >
-      <input
-        ref={inputRef}
-        disabled={isDisabled}
-        placeholder={t("search")}
-        className="h-full w-full bg-transparent outline-hidden text-sm dark:text-white"
-        type="search"
-        value={value}
-        onChange={onChange}
-      />
       <div
         onClick={handleFocusInput}
         role="button"
@@ -55,6 +72,28 @@ export default function SearchField({
       >
         <IconSearch className="text-gray-500 dark:text-white" size={14} />
       </div>
+      <input
+        {...props}
+        ref={inputRef}
+        disabled={isDisabled}
+        placeholder={t("search")}
+        className="h-full w-full bg-transparent outline-hidden text-sm dark:text-white"
+        type="search"
+        value={value}
+        onChange={onChange}
+        onKeyDown={handleKeyDown}
+      />
+      {!!value && !!onClear && !!isClearable && (
+        <button
+          aria-label="clear"
+          onClick={handleClear}
+          role="button"
+          type="button"
+          className="w-8 h-8 flex justify-center items-center transition-colors duration-100 hover:bg-gray-300/20 rounded"
+        >
+          <IconX size={16} className="text-gray-500 dark:text-white" />
+        </button>
+      )}
     </div>
   );
 }

c910a8417fbb4ffe3f7e8ac909f1ee58ac94fa2b
Theo Billardey <toto.billardey@gmail.com>
2025-03-28 18:36:22 +0100

    feat(components): handle search in list header

--- src/components/ListFilterHeader/listfilterheader.d.ts
+++ src/components/ListFilterHeader/listfilterheader.d.ts
@@ -1,9 +1,12 @@
 import type { SelectOption } from "@/components/Select";
 
+import { PageSearchParams } from "@/types";
+
 export type ListFilterHeaderProps = {
   textSearch?: string;
   sortOptionList?: SelectOption[];
   resultCount: number;
   activeFilterCount: number;
+  pageSearchParams: PageSearchParams;
   handleOpenFilterDrawer: () => void;
 };

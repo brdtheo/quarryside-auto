79b36148047ca66ba61e45f1b7901ea11ab21f86
Theo Billardey <toto.billardey@gmail.com>
2025-04-04 14:57:27 +0200

    test(components): fix media list flaky test
    Ensure at least one thumbnail is in the media list factory

--- src/lib/media/factory.ts
+++ src/lib/media/factory.ts
@@ -2,17 +2,20 @@ import { Media } from "@prisma/client";
 
 import { faker } from "@faker-js/faker";
 
-export function mediaFactory(): Media {
+export function mediaFactory(options?: { isThumbnail: boolean }): Media {
   return {
     id: faker.number.int({ max: 1000 }),
     vehicle: faker.helpers.arrayElement([null, faker.number.int()]),
     wheel: faker.helpers.arrayElement([null, faker.number.int()]),
     url: faker.image.url({ width: 1000, height: 562 }),
-    is_thumbnail: faker.datatype.boolean(),
+    is_thumbnail: options?.isThumbnail ?? false,
     date_created: faker.date.recent(),
   };
 }
 
 export function mediaListFactory(count: number = 5): Media[] {
-  return faker.helpers.multiple(() => mediaFactory(), { count });
+  return [
+    mediaFactory({ isThumbnail: true }),
+    ...faker.helpers.multiple(() => mediaFactory(), { count }),
+  ];
 }

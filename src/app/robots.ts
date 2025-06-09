e78a6707ab4ce08b029e9488e0aa6ad7c10ac1a7
Theo Billardey <toto.billardey@gmail.com>
2025-06-09 17:05:53 +0200

    fix(robots): disallow query param urls
    Prevent bots from crawling urls that contain query params

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -7,6 +7,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: "*",
       allow: "/",
+      disallow: ["/*?*"],
     },
     sitemap: `${DOMAIN_URL}/sitemap.xml`,
   };

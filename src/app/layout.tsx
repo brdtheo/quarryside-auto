18ef897747f73432c677fedcc833de30207af28c
Theo <toto.billardey@gmail.com>
2026-04-13 21:41:45 +0200

    feat(analytics): replace plausible by openpanel for analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,14 @@
-import { OpenPanelComponent } from "@openpanel/nextjs";
 import { ReactNode } from "react";
 
-type Props = {
+import { OpenPanelComponent } from "@openpanel/nextjs";
+
+type Properties = {
   children: ReactNode;
 };
 
 // Since we have a `not-found.tsx` page on the root, a layout file
 // is required, even if it's just passing children through.
-export default function RootLayout({ children }: Props) {
+export default function RootLayout({ children }: Properties) {
   return (
     <>
       <OpenPanelComponent

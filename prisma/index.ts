3350ec0e8013ca36d74fdc07d6ef4bfdb48e0cc5
Theo <toto.billardey@gmail.com>
2026-07-03 19:20:38 +0200

    build(prisma): update configuration and schema

---
+++ prisma/index.ts
@@ -0,0 +1,9 @@
+import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
+
+import { PrismaClient } from "./generated/client";
+
+const adapter = new PrismaBetterSqlite3({
+  url: "file:./db/dev.db",
+});
+
+export const prisma = new PrismaClient({ adapter });

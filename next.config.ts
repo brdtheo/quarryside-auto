7385d3e029f4e6d78c83ca539a5bc842f75dae92
Theo <toto.billardey@gmail.com>
2026-07-03 23:31:23 +0200

    chore(sentry): update configuration

--- next.config.ts
+++ next.config.ts
@@ -48,6 +48,7 @@ export default withSentryConfig(withNextIntl(nextConfig), {
   // https://www.npmjs.com/package/@sentry/webpack-plugin#options
 
   org: "brdtheo",
+
   project: "quarryside-auto",
 
   // Only print logs for uploading source maps in CI
@@ -59,23 +60,9 @@ export default withSentryConfig(withNextIntl(nextConfig), {
   // Upload a larger set of source maps for prettier stack traces (increases build time)
   widenClientFileUpload: true,
 
-  // Automatically annotate React components to show their full name in breadcrumbs and session replay
-  reactComponentAnnotation: {
-    enabled: true,
-  },
-
   // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
   // This can increase your server load as well as your hosting bill.
   // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
   // side errors will fail.
   tunnelRoute: "/monitoring",
-
-  // Automatically tree-shake Sentry logger statements to reduce bundle size
-  disableLogger: true,
-
-  // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
-  // See the following for more information:
-  // https://docs.sentry.io/product/crons/
-  // https://vercel.com/docs/cron-jobs
-  automaticVercelMonitors: true,
 });

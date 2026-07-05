a8229be461e58f4f53d63e01d7bf5ae574c18e4a
Theo <toto.billardey@gmail.com>
2026-07-05 15:46:22 +0200

    refactor(components): replace pricing symbol by beam bucks

--- src/lib/wheel/WheelCard/WheelCard.tsx
+++ src/lib/wheel/WheelCard/WheelCard.tsx
@@ -7,6 +7,8 @@ import {
   IconTruckDelivery,
 } from "@tabler/icons-react";
 
+import Beambucks from "@/components/Beambucks";
+
 import MediaSkeleton from "@/lib/media/MediaSkeleton";
 import useWheelDetails from "@/lib/wheel/hooks/useWheelDetails";
 
@@ -22,12 +24,12 @@ export default function WheelCard({ wheel }: WheelCardProps) {
   const { title, price, href, brand } = useWheelDetails(wheel);
 
   return (
-    <article className="@container/wheelcard w-full @md/wheelcard:w-52">
+    <article className="@container/wheelcard w-full @md/wheelcard:w-52 h-full">
       <Link
         href={href}
-        className="border border-grey rounded flex w-full @md/wheelcard:w-52 bg-white dark:bg-blacksecondary dark:border-blacksecondary bg-clip-content overflow-hidden"
+        className="border border-grey rounded flex w-full h-full @md/wheelcard:w-52 bg-white dark:bg-blacksecondary dark:border-blacksecondary bg-clip-content overflow-hidden"
       >
-        <div className="w-full flex flex-col gap-2">
+        <div className="w-full h-full flex flex-col gap-2">
           <div className="w-full flex self-center h-48 @sm/wheelcard:h-60 relative">
             {!thumbnail && <MediaSkeleton className="w-full h-full" />}
             {!!thumbnail && (
@@ -41,37 +43,40 @@ export default function WheelCard({ wheel }: WheelCardProps) {
             )}
           </div>
 
-          <div className="flex flex-col gap-2 px-4 pt-2 pb-4">
-            <span className="font-semibold text-xs text-grey-secondary leading-none">
-              {brand}
-            </span>
-            <span className="font-medium text-lg leading-6 pb-1">
-              {wheel.model}
-            </span>
-            {(wheel.delivery_available || wheel.free_on_site_pickup) && (
-              <ul className="flex flex-col gap-1">
-                {wheel.free_on_site_pickup && (
-                  <li className="inline-flex items-start gap-1">
-                    <IconBuildingStore stroke={1.6} size={18} />
-                    <span className="font-medium text-xs">
-                      {t("filter.free_on_site_pickup.option.true")}
-                    </span>
-                  </li>
-                )}
-                {wheel.delivery_available && (
-                  <li className="inline-flex items-start gap-1">
-                    <IconTruckDelivery stroke={1.6} size={18} />
-                    <span className="font-medium text-xs">
-                      {t("filter.delivery_available.option.true")}
-                    </span>
-                  </li>
-                )}
-              </ul>
-            )}
-            <div className="flex border border-yellow mt-1">
+          <div className="flex flex-col flex-1 justify-between px-4 pt-2 pb-4">
+            <div className="flex flex-col flex-1 gap-2">
+              <span className="font-semibold text-xs text-grey-secondary leading-none">
+                {brand}
+              </span>
+              <span className="font-medium text-lg leading-6 pb-1">
+                {wheel.model}
+              </span>
+              {(wheel.delivery_available || wheel.free_on_site_pickup) && (
+                <ul className="flex flex-col gap-1">
+                  {wheel.free_on_site_pickup && (
+                    <li className="inline-flex items-start gap-1">
+                      <IconBuildingStore stroke={1.6} size={18} />
+                      <span className="font-medium text-xs">
+                        {t("filter.free_on_site_pickup.option.true")}
+                      </span>
+                    </li>
+                  )}
+                  {wheel.delivery_available && (
+                    <li className="inline-flex items-start gap-1">
+                      <IconTruckDelivery stroke={1.6} size={18} />
+                      <span className="font-medium text-xs">
+                        {t("filter.delivery_available.option.true")}
+                      </span>
+                    </li>
+                  )}
+                </ul>
+              )}
+            </div>
+
+            <div className="flex border border-yellow mt-3">
               <div className="bg-yellow flex flex-1 justify-center items-start px-1">
-                <span className="text-red font-bold text-normal">$</span>
-                <span className="text-red font-extrabold text-2xl">
+                <span className="text-red font-bold text-2xl">
+                  <Beambucks className="text-xl mr-0.5" />
                   {price}
                 </span>
               </div>

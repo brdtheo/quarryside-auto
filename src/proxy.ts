ef30ade4a96237085cd1bf2280ea150ea5049778
Theo <toto.billardey@gmail.com>
2026-07-05 14:58:08 +0200

    refactor(next-intl): update configuration

--- src/proxy.ts
+++ src/proxy.ts
@@ -5,17 +5,8 @@ import { routing } from "./i18n/routing";
 export default createMiddleware(routing);
 
 export const config = {
-  // // Match only internationalized pathnames
-  // matcher: ["/", "/(ru|en)/:path*", "/:path*"],
-
-  // Matcher entries are linked with a logical "or", therefore
-  // if one of them matches, the middleware will be invoked.
-  matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    String.raw`/((?!_next|monitoring|[^?]*\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|xml|txt|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)`,
-    // Match all pathnames except for
-    // - … if they start with `/api`, `/_next` or `/_vercel`
-    // - … the ones containing a dot (e.g. `favicon.ico`)
-    String.raw`/((?!api|_next|_vercel|.*\..*).*)`,
-  ],
+  // Match all pathnames except for
+  // - … if they start with `/api`, `/trpc`, `/_next` or `/_vercel`
+  // - … the ones containing a dot (e.g. `favicon.ico`)
+  matcher: "/((?!api|trpc|_next|_vercel|.*\\..*).*)",
 };

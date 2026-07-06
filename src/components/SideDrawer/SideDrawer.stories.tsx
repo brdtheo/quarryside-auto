f506beec35935f5195ba82db9f183a8da8093a2e
Theo <toto.billardey@gmail.com>
2026-07-06 13:15:12 +0200

    build(storybook): migrate to v10

--- src/components/SideDrawer/SideDrawer.stories.tsx
+++ src/components/SideDrawer/SideDrawer.stories.tsx
@@ -1,7 +1,7 @@
 import { faker } from "@faker-js/faker";
 
 import type { Meta, StoryObj } from "@storybook/react";
-import { fn } from "@storybook/test";
+import { fn } from "storybook/test";
 
 import SideDrawer from ".";
 
